5994f4f537dbbe9452c246214fb4a7e91d23f989
leejunwon03-oss <leejunwon03@kookmin.ac.kr>
2025-09-23 18:45:57 +0900

    Add files via upload

---
+++ 05_practice_2.ino
@@ -0,0 +1,22 @@
+void setup() {
+  pinMode(7, OUTPUT);
+  digitalWrite(7, HIGH);
+}
+
+void loop() {
+  
+  digitalWrite(7, HIGH);
+  delay(1000);
+
+  
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(7, LOW);   
+    delay(100);
+    digitalWrite(7, HIGH);  
+    delay(100);
+  }
+
+  digitalWrite(7, LOW);
+
+  while (1) {}  
+}
